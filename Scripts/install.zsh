8f2b38cc45f07c8c55bd2698088168e60551e35d
ErnstBecker <ernst.becker.neto@gmail.com>
2024-09-06 01:06:41 -0300

    :recycle: Update the install script

--- Scripts/install.zsh
+++ Scripts/install.zsh
@@ -1,46 +1,22 @@
-#!/bin/zsh
+#! /bin/zsh
 
+# ? Get the package manager of your choice (paru or yay) and install the packages
+read "package_manager?Choose your package manager? (Enter 'paru' or 'yay'): "
 
-echo 'Installing dotfiles...'
+if [[ "$package_manager" != "paru" && "$package_manager" != "yay" ]]; then
+	echo "Wrong option."
+	exit 1
+fi
+
+# ! Fonts and Theme
+$package_manager -S inter-font noto-fonts-cjk noto-fonts-emoji noto-fonts ttf-cascadia-code-nerd ttf-firacode-nerd ttf-nerd-fonts-symbols papirus-icon-theme papirus-folders-git
+papirus-folders -C yaru --theme Papirus-Dark
 
 # ! Zsh
-echo 'Ricing Zsh...'
-cp -r config/zsh ~/.config/zsh
-echo 'Installing zsh-autosuggestions'
-git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.config/zsh/zsh-autosuggestions
-echo 'Installing sh-syntax-highlighting'
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.config/zsh/zsh-syntax-highlighting
-ln -s ~/.config/zsh/zshrc ~/.zshrc
-echo 'Zsh done!'
+$package_manager -S zsh zoxide ohmyposh zsh-syntax-highlighting zsh-autosuggestions
 
-# ! Oh-My-Posh
-echo 'Ricing Oh-My-Posh...'
-paru -S oh-my-posh
-mv -f ~/.config/ohmyposh ~/.config/ohmyposh.old
-cp -f config/ohmyposh ~/.config/ohmyposh
+# ! Programs
+$package_manager -S visual-studio-code-bin spotify docker docker-desktop vesktop-bin kitty mpv fastfetch cava pipes.sh ani-cli
 
-# ! Fastfetch
-echo 'Ricing Fastfetch...'
-mv -f ~/.config/fastfetch ~/.config/fastfetch.old
-cp -r config/fastfetch ~/.config/fastfetch
-echo 'Fastfetch done!'
-
-# ! Kitty
-echo 'Ricing Kitty...'
-mv -f ~/.config/kitty ~/.config/kitty.old
-cp -r config/kitty ~/.config/kitty
-echo 'Kitty done!'
-
-# ! Cava
-echo 'Ricing Cava...'
-mv -f ~/.config/cava ~/.config/cava.old
-cp -r config/cava ~/.config/cava
-echo 'Cava done!'
-
-# ! Rofi
-echo 'Ricing rofi...'
-mv -f ~/.config/rofi ~/.config/rofi.old
-cp -r config/rofi ~/.config/rofi
-echo 'Rofi done!'
-
-echo 'Dotfiles installed successfully!'
+# ! Cursors
+$package_manager -Scolloid-cursors-git
