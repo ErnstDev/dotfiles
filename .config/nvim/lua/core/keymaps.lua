bd5a091563f1866fda8272e342bfea61933a842d
ErnstBecker <ernst.becker.neto@gmail.com>
2025-01-05 07:59:34 -0300

    :wrench: Add select all keymap

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,1 +1,8 @@
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+-- Select all
+vim.keymap.set('n', '<C-a>', 'ggVG')
+
+-- NvimTree
+vim.keymap.set('n', '<C-b>', ':NvimTreeToggle<CR>')
+vim.keymap.set('n', '<Leader>e', ':NvimTreeFindFile<CR>')
