92a403b409359029c7b6982bef61885d3d78f565
ErnstBecker <ernst.becker.neto@gmail.com>
2025-01-05 10:39:28 -0300

    :recycle: Restructure neovim configuration

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,19 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    lazyrepo,
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 require("core.options")
 require("core.keymaps")
-require("core.plugins")
 
-
-require("core.configs.colorscheme")
-require("core.configs.telescope")
+require("lazy").setup("plugins")
--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,8 +1,4 @@
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-
 -- Select all
 vim.keymap.set('n', '<C-a>', 'ggVG')
 
--- NeoTree
-vim.keymap.set('n', '<C-b>', ':Neotree filesystem toggle right<CR>')
-
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
