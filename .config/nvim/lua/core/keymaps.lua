a4ac2a3fe9e9217a023120b8a230dc59682694ff
ErnstBecker <ernst.becker.neto@gmail.com>
2025-01-12 12:29:03 -0300

    :recycle: Add toggle function to normal & insert modes

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,3 +1,7 @@
+-- Set toggle modes ( Insert & Normal )
+vim.keymap.set('n', '<Esc>', 'i', { noremap = true})
+vim.keymap.set('i', '<Esc>', '<Esc>', { noremap = true })
+
 -- Select all
 vim.keymap.set('n', '<C-a>', 'ggVG')
 
