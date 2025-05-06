be04fabc8c6056b777d00e258d4854be7a5fb626
ErnstBecker <ernst.becker.neto@gmail.com>
2025-05-06 20:26:26 +0000

    :rotating_light: New installation script in WIP

--- Scripts/install.zsh
+++ Scripts/install.zsh
@@ -1,7 +1,7 @@
 #! /bin/zsh
 
 # ? Get the package manager of your choice (paru or yay) and install the packages
-read "package_manager?Choose your package manager? (Enter 'paru' or 'yay'): "
+read "package_manager? Choose your package manager? (Enter 'paru' or 'yay'): "
 
 if [[ "$package_manager" != "paru" && "$package_manager" != "yay" ]]; then
 	echo "Wrong option."
@@ -13,10 +13,13 @@ $package_manager -S inter-font noto-fonts-cjk noto-fonts-emoji noto-fonts ttf-ca
 papirus-folders -C yaru --theme Papirus-Dark
 
 # ! Zsh
-$package_manager -S zsh zoxide lsd bat ohmyposh 
+$package_manager -S zsh zoxide lsd bat oh-my-posh-bin 
 
-# ! Programs
-$package_manager -S visual-studio-code-bin neovim spotify vesktop-bin kitty mpv fastfetch cava pipes.sh ani-cli
+# ! TUI & CLI 
+$package_manager -S neovim tty-clock btop ranger ani-cli fastfetch cava pipes.sh
 
-# ! Cursors
-$package_manager -S colloid-cursors-git
+# ! DEV
+$package_manager -S visual-studio-code-bin postman-bin spotify slack-desktop
+
+# ! ETC
+$package_manager vesktop-bin zen-browser-bin
